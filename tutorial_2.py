4e289b8a0e01b71e64a7bb9ed8c1a71a711f5084
Ellis Lee <ellismckenzielee@gmail.com>
2019-01-23 12:54:13 +0000

    completed sentdex OOP Crash Course with Python 3 and Tkinter tutorial

---
+++ tutorial_2.py
@@ -0,0 +1,42 @@
+import tkinter as tk
+
+LARGE_FONT =('Verdana', 12)
+
+class SeapfBTCAPP(tk.Tk):
+    
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        container = tk.Frame(self)
+
+        #Fill fills in space that you have alloted the pack
+        #Expand fills any potential space
+        container.pack(side='top', fill='both', expand = True)
+
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+
+        frame = StartPage(container, self)
+
+        self.frames[StartPage] = frame
+
+        #nsew stretches frame to the size of the window 
+        frame.grid(row=0, column=0, sticky='nsew')
+
+        self.show_frame(StartPage)
+        print(StartPage)
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
+
+class StartPage(tk.Frame):
+
+    def __init__(self, parent, cont):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text='Start Page', font=LARGE_FONT)
+        label.pack(pady=10,padx=10)
+
+
+app = SeapfBTCAPP()
+app.mainloop()
